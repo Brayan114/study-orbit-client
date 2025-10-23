42f49f02990cbb36f5bb86c08063d41cf6c031c6
Brayan Osinaka <brayanjunior101@gmail.com>
2025-10-23 18:45:23 +0100

    Initial commit: Project setup with routing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import './App.css'; // We'll keep the import for later
+
+function App() {
+  return (
+    <div>
+      <h1>StudyOrbit</h1>
+    </div>
+  );
+}
+
+export default App;
